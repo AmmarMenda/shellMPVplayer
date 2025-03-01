cb74757a25cb72bfabb6715d8604b5a2d8bf73bd
Ammar Menda <ammar.menda0@gmail.com>
2025-03-01 23:49:01 +0530

    Fixed some stupid things i did

--- main.sh
+++ main.sh
@@ -3,12 +3,12 @@
 while true; do
     echo "1: Shuffle Play"
     echo "2: List Play"
-    read -p "Enter your choice: " ch 
+    read -p "Enter your choice: " ch
 
     if [ "$ch" -eq 1 ]; then  
-        source /home/ammar/clones/shufflerForMpv/shuffle.sh
+        source /home/ammar/clones/shellMPVplayer/shuffle.sh
     elif [ "$ch" -eq 2 ]; then 
-        source /home/ammar/clones/shufflerForMpv/listplay.sh
+        source /home/ammar/clones/shellMPVplayer/listplay.sh
     else
         echo "Invalid choice. Please try again."  
     fi
